0fd27a9417ed96510136e434fe41065cfa82b76a
iamDuckduck <simonchungCCK42@gmail.com>
2024-06-16 22:22:30 +0900

    Ship genres with the app

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -1,10 +1,10 @@
-import useData from "./useData";
+import genres from "../data/genres.json";
 
 export interface Genre {
   mal_id: number;
   name: string;
 }
 
-const useGenres = () => useData<Genre>("/genres/anime");
+const useGenres = () => ({ data: genres.data, isLoading: false, error: null });
 
 export default useGenres;
