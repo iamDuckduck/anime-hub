398b0ae8817daa8748e998dc92b03aaa5d4f61f2
iamDuckduck <simonchungCCK42@gmail.com>
2024-06-16 22:31:01 +0900

    Refactor GameGrid

--- src/component/GameGrid.tsx
+++ src/component/GameGrid.tsx
@@ -14,9 +14,9 @@ const GameGrid = ({ animeQuery }: Props) => {
   const { data, error, isLoading } = useAnimes(animeQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
