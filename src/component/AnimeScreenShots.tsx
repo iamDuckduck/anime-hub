0df30c6f7f2d5dac0c26c26fe979590db8eb13f7
iamDuckduck <simonchungCCK42@gmail.com>
2024-06-25 20:39:13 +0900

    Build game screenshot

---
+++ src/component/AnimeScreenShots.tsx
@@ -0,0 +1,27 @@
+import useAnimeScreenShot from "../hooks/useAnimeScreenShot";
+import { SimpleGrid, Spinner, Image } from "@chakra-ui/react";
+interface Props {
+  animeId: number;
+}
+const AnimeScreenShots = ({ animeId }: Props) => {
+  const { data, isLoading, error } = useAnimeScreenShot(String(animeId));
+  const imageList = data?.data;
+  console.log(imageList);
+  if (isLoading) return <Spinner></Spinner>;
+
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing="10px" padding="20px">
+      {imageList?.map((image) => (
+        <Image
+          key={image.jpg.large_image_url}
+          src={image.jpg.large_image_url}
+          width="100%"
+        ></Image>
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default AnimeScreenShots;
