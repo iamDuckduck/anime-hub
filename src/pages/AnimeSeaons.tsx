e5cafadb18242a6c382b4fd067d2b8ab4e35c94b
iamDuckduck <simonchungCCK42@gmail.com>
2024-07-15 16:10:14 +0900

    create tag and selectors

--- src/pages/AnimeSeaons.tsx
+++ src/pages/AnimeSeaons.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 
 const AnimeSeaons = () => {
-  return <div>AnimeSeaons</div>;
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+          Seasons
+        </MenuButton>
+        <MenuList maxHeight="200px" overflowY="scroll">
+          <MenuItem>Download</MenuItem>
+          <MenuItem>Create a Copy</MenuItem>
+          <MenuItem>Mark as Draft</MenuItem>
+          <MenuItem>Delete</MenuItem>
+          <MenuItem>Attend a Workshop</MenuItem>
+        </MenuList>
+      </Menu>
+
+      <Tabs paddingY={5}>
+        <TabList>
+          <Tab>Spring</Tab>
+          <Tab>Summer</Tab>
+          <Tab>Fall</Tab>
+          <Tab>Winter</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 };
 
 export default AnimeSeaons;
