58fe50e0cb02ffd95fee911668b055dc3a559395
iamDuckduck <simonchungCCK42@gmail.com>
2024-07-15 17:20:53 +0900

    add useSeasonAnime query

---
+++ src/hooks/useSeasonAnimes.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import Anime from "../entities/Anime";
+import APIClient from "../services/api-client";
+import ms from "ms";
+
+const apiClient = new APIClient<Anime>("/seasons");
+
+const useSeasonAnimes = (year: string, season: string) => {
+  return useQuery({
+    queryKey: ["SeasonAnimes", { year: year, season: season }],
+    queryFn: () => apiClient.getSeasonAnime(year, season),
+    staleTime: ms("24h"),
+  });
+};
+
+export default useSeasonAnimes;
