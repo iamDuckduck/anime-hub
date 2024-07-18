2796db0044c6e698b00df92bba43885edc5c09ca
iamDuckduck <simonchungCCK42@gmail.com>
2024-07-19 02:55:19 +0900

    temp fix sidebar button not working in mb ver

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,27 +1,10 @@
-import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
 import SideBar from "../component/SideBar";
-import { useMenuBarToggleStore } from "../store";
 
 const HomeLayout = () => {
-  const isOpen = useMenuBarToggleStore((s) => s.isOpen);
-
   return (
     <>
-      {isOpen && (
-        <Box
-          position="fixed"
-          zIndex="1"
-          backgroundColor="black"
-          width="100vw"
-          height="100vh"
-          top="0"
-          left="0"
-          padding={5}
-        >
-          <SideBar></SideBar>
-        </Box>
-      )}
       <Grid
         templateAreas={{
           base: `"main"`,
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,28 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../component/navBar";
+import SideBar from "../component/SideBar";
+import { useMenuBarToggleStore } from "../store";
 
 const Layout = () => {
+  const isOpen = useMenuBarToggleStore((s) => s.isOpen);
+
   return (
     <>
+      {isOpen && (
+        <Box
+          position="fixed"
+          zIndex="1"
+          backgroundColor="black"
+          width="100vw"
+          height="100vh"
+          top="0"
+          left="0"
+          padding={5}
+        >
+          <SideBar></SideBar>
+        </Box>
+      )}
       <NavBar></NavBar>
       <Box padding={5}>
         <Outlet></Outlet>
