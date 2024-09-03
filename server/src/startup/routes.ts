8d3d89a4a5a17718d62adfe78101023d708615cb
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-03 16:46:18 +0800

    added routes test and favorite

--- server/src/startup/routes.ts
+++ server/src/startup/routes.ts
@@ -1,8 +1,14 @@
 import { Express } from "express";
 import bodyParser from "body-parser";
 import { router as users } from "../route/users";
+import { router as favorites } from "../route/favorites";
+import { router as test } from "../route/tests";
+import error from "../middleware/error";
 
 export default function (app: Express) {
   app.use(bodyParser.json());
   app.use("/api/users", users);
+  app.use("/api/favorites", favorites);
+  app.use("/api/tests", test);
+  app.use(error);
 }
