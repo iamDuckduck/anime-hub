b36320d53c9e9eb78621dfc73abae2281ee20a12
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-09 19:37:39 +0800

    delete useless code

--- server/src/models/watchList.ts
+++ server/src/models/watchList.ts
@@ -2,24 +2,6 @@ import { Document, Schema, model, Model, Types } from "mongoose";
 import mongoose from "mongoose";
 import Joi from "joi";
 
-// enum Status {
-//   Watching = "Watching",
-//   Rewatching = "Rewatching",
-//   Completed = "Completed",
-//   Paused = "Paused",
-//   Dropped = "Dropped",
-//   Planning = "Planning",
-// }
-
-// interface Anime {
-//   animeId: Number;
-//   title: String;
-//   imageUrl: String;
-//   genre: String;
-//   totalEpisodes: number;
-//   score: number;
-// }
-
 //extend the document interface,
 export interface watchListDoc extends Document {
   _id: Types.ObjectId;
@@ -32,12 +14,6 @@ const watchListSchema = new Schema<watchListDoc>({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    // validate: {
-    //   validator: function (value: string) {
-    //     return mongoose.Types.ObjectId.isValid(value);
-    //   },
-    //   message: (props) => `${props.value} is not a valid ObjectId!`,
-    // },
   },
   name: {
     type: String,
@@ -60,16 +36,18 @@ const WatchList = model<watchListDoc, Model<watchListDoc>>(
 
 const validateWatchList = (WatchList: object) => {
   const schema = Joi.object({
-    userId: Joi.string().custom(
-      //objectID will be converted to string in http requests
-      (value: string, helpers: Joi.CustomHelpers<any>) => {
-        if (mongoose.Types.ObjectId.isValid(value)) return value;
-        else
-          return helpers.message({
-            custom: `${value} is not a valid ObjectId!`,
-          });
-      }
-    ),
+    userId: Joi.string()
+      .custom(
+        //objectID will be converted to string in http requests
+        (value: string, helpers: Joi.CustomHelpers<any>) => {
+          if (mongoose.Types.ObjectId.isValid(value)) return value;
+          else
+            return helpers.message({
+              custom: `${value} is not a valid ObjectId!`,
+            });
+        }
+      )
+      .required(),
     name: Joi.string().min(1).max(80).required(),
   });
 
