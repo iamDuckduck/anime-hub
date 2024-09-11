44422257a7049ab9d959bd617a4afd38cc0bb71d
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-11 16:43:36 +0800

    added cor so i can test it in localhost *will be deleted before production or disable it

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,22 @@ import "express-async-errors";
 import { logger, handleRejection } from "./startup/logger";
 import routes from "./startup/routes";
 import mongodb from "./startup/mongodb";
+const cors = require("cors");
 
 const app = express();
+
+// Set up CORS with custom options
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow requests from this origin
+  methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
+  allowedHeaders: "Content-Type,Authorization", // Allow these headers
+};
+app.use(cors(corsOptions));
 handleRejection(); //it handles unhandled rejected promise
 routes(app); //set up all the routes
 mongodb(); // connect to mongodb
 
+console.log("key" + config.get("jwtPrivateKey"));
 const port: number = config.get("port");
 //port will be null if in test env
 port
