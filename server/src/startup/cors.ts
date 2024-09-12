d10ae0a4ea2924e711a5182db7d229af5e37e5a9
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-12 19:07:40 +0800

    added x-auth-token as allowed header

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -6,7 +6,7 @@ export const enableCors = function (app: Express) {
   const corsOptions = {
     origin: "http://localhost:5173", // Allow requests from this origin
     methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-    allowedHeaders: "Content-Type,Authorization", // Allow these headers
+    allowedHeaders: "Content-Type,Authorization,x-auth-token", // Allow these headers
   };
 
   app.use(cors(corsOptions));
