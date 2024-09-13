7178b13bf09f54f570573963d1bcf39d0cf99145
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-14 06:54:34 +0800

    added route for uploading image to cloudinary

---
+++ server/src/router/uploadImage.ts
@@ -0,0 +1,38 @@
+import Express from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import { Request, Response } from "express";
+import { auth } from "../middleware/auth";
+const router = Express.Router();
+
+cloudinary.v2.config({
+  //hard code for now
+  cloud_name: "drbighiyo",
+  api_key: "211846568947739",
+  api_secret: "GV8vFUH_DL8mgqRpeiKrt6OD_w4",
+});
+
+//init Multer
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Upload endpoint
+router.post(
+  "/",
+  [auth, upload.single("image")],
+  async (req: Request, res: Response) => {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded." });
+    }
+    // Upload the image to Cloudinary
+    await cloudinary.v2.uploader
+      .upload_stream({ resource_type: "image" }, (error, result) => {
+        if (error) {
+          return res.status(500).json({ message: "Failed to upload image." });
+        }
+        res.json({ imageUrl: result?.secure_url });
+      })
+      .end(req.file.buffer); // Send file buffer to Cloudinary
+  }
+);
+
+export { router };
