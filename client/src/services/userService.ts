a337ffbf1ea16a72e05a47486c247819a9b54da2
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-14 06:54:12 +0800

    added userService api client for posting to db

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -11,32 +11,11 @@ class APIClient<T, D> {
     this.endpoint = endpoint;
   }
 
-  post = (data: T) => {
-    return axiosInstance.post<D>(this.endpoint, data).then((res) => res.data);
+  post = (data: T, headers?: Record<string, string>) => {
+    return axiosInstance
+      .post<D>(this.endpoint, data, { headers: headers })
+      .then((res) => res.data);
   };
-
-  // use arrow function so no this.endpoint reference problem
-  //   getAll = (config: AxiosRequestConfig) => {
-  //     return axiosInstance
-  //       .get<FetchResponse<T>>(this.endpoint, config)
-  //       .then((res) => res.data);
-  //   };
-
-  //   get = (id: string | number) => {
-  //     return axiosInstance
-  //       .get<FetchAnimeResponse<T>>(this.endpoint + "/" + id)
-  //       .then((res) => res.data);
-  //   };
-
-  //   getSeasonAnime = (
-  //     year: string,
-  //     season: string,
-  //     config: AxiosRequestConfig
-  //   ) => {
-  //     return axiosInstance
-  //       .get<FetchResponse<T>>(this.endpoint + "/" + year + "/" + season, config)
-  //       .then((res) => res.data);
-  //   };
 }
 
 export default APIClient;
