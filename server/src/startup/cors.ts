743272fee2b7471b4ecd5be327019bab523b39d6
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-14 06:11:38 +0800

    fixed error

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  if (process.env.NODE_ENV == " development") {
+  if (process.env.NODE_ENV == "development") {
     // Set up CORS with custom options
     const corsOptions = {
       origin: "http://localhost:5173", // Allow requests from this origin
