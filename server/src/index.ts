5304cc1ff75ac0157f0a1dac0d5fca0f93ec77d4
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-14 06:09:56 +0800

    refactor index.ts, apply cors only in development, delete port in default.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,26 @@
 import express from "express";
-import config from "config";
 import "express-async-errors";
 import { logger, handleRejection } from "./startup/logger";
 import routes from "./startup/routes";
 import mongodb from "./startup/mongodb";
 import { enableCors } from "./startup/cors";
+import config from "config";
+
+// ugly way to check private key variable is setted or not
+if (!config.get("jwtPrivateKey")) {
+  console.log("setup failed");
+}
 
 const app = express();
 const env = process.env.NODE_ENV;
 
 handleRejection(); //it handles unhandled rejected promise
+enableCors(app); //enable cors when it's in development
 routes(app); //set up all the routes
 mongodb(); // connect to mongodb
 
-env == "development" ? enableCors(app) : ""; //enable cors so i can test it in localhost (need to be deleted in production)
-
-env == ("development" || "production")
+env !== "test"
   ? app.listen(3000, () => logger.info(`Listening on port 3000...`))
-  : logger.info(`not listening to any port (test env)`);
+  : logger.info(`not listening to any port (test env)`); //we need to dynamically assign ports when testing api
 
 export { app };
--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,11 +2,13 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  // Set up CORS with custom options
-  const corsOptions = {
-    origin: "http://localhost:5173", // Allow requests from this origin
-    methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-    allowedHeaders: "Content-Type,Authorization,x-auth-token", // Allow these headers
-  };
-  app.use(cors(corsOptions));
+  if (process.env.NODE_ENV == " development") {
+    // Set up CORS with custom options
+    const corsOptions = {
+      origin: "http://localhost:5173", // Allow requests from this origin
+      methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
+      allowedHeaders: "Content-Type,Authorization,x-auth-token", // Allow these headers
+    };
+    app.use(cors(corsOptions));
+  }
 };
