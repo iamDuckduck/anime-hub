0556527d954200462ac403c7acaaa722b0ef2b90
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-17 12:06:16 +0800

    added session setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import routes from "./startup/routes";
 import mongodb from "./startup/mongodb";
 import { enableCors } from "./startup/cors";
 import config from "config";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 // ugly way to check private key variable is setted or not
 if (!config.get("jwtPrivateKey")) {
@@ -13,11 +15,22 @@ if (!config.get("jwtPrivateKey")) {
 
 const app = express();
 const env = process.env.NODE_ENV;
+const db: string = config.get("db");
 
 handleRejection(); //it handles unhandled rejected promise
 enableCors(app); //enable cors when it's in development
-routes(app); //set up all the routes
 mongodb(); // connect to mongodb
+app.use(
+  session({
+    secret: "your-secret-key", // Change this to a secure random string
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: db }),
+    cookie: { maxAge: 15 * 60 * 1000 }, // 15 minutes
+  })
+);
+
+routes(app); //set up all the routes
 
 env !== "test"
   ? app.listen(3000, () => logger.info(`Listening on port 3000...`))
