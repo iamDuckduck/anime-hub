6681db07377a23546dec9e7a3631a8a032f82751
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-21 17:23:16 +0800

    clean up code

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,31 +5,21 @@ import routes from "./startup/routes";
 import mongodb from "./startup/mongodb";
 import { enableCors } from "./startup/cors";
 import config from "config";
-import session from "express-session";
-import MongoStore from "connect-mongo";
+import { enableSession } from "./startup/session";
 
 // ugly way to check private key variable is setted or not
 if (!config.get("jwtPrivateKey")) {
   console.log("setup failed");
+  process.exit(1);
 }
 
 const app = express();
 const env = process.env.NODE_ENV;
-const db: string = config.get("db");
 
-handleRejection(); //it handles unhandled rejected promise
-enableCors(app); //enable cors when it's in development
+handleRejection(); //it handles unhandled rejected promise outside the route scope
+enableCors(app); //enable cors when it's in development, disable in production
 mongodb(); // connect to mongodb
-app.use(
-  session({
-    secret: "your-secret-key", // Change this to a secure random string
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: db }),
-    cookie: { maxAge: 15 * 60 * 1000 }, // 15 minutes
-  })
-);
-
+enableSession(app); // enable session
 routes(app); //set up all the routes
 
 env !== "test"
