5f91fc53c2ff5b665d79654d2384d4465021a663
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-24 01:04:57 +0800

    added method for handling different api request

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,19 +1,52 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
+
+export interface userData {
+  userName: string;
+  email: string;
+  profileImage: string;
+  bannerImage: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isAdmin: Boolean;
+  _id: string;
+}
+
+export interface CloudinaryRes {
+  imageUrl: string;
+}
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api/",
 });
 
-class APIClient<T, D> {
+class APIClient<T, D = unknown> {
   endpoint: string;
 
   constructor(endpoint: string) {
     this.endpoint = endpoint;
   }
 
-  post = (data: T, headers?: Record<string, string>) => {
+  post = (data?: T, headers?: Record<string, string>) => {
     return axiosInstance
-      .post<D>(this.endpoint, data, { headers: headers })
+      .post<D>(this.endpoint, data, { headers, withCredentials: true })
+      .then((res) => res.data);
+  };
+
+  get = () => {
+    return axiosInstance
+      .get<userData>(this.endpoint, { withCredentials: true })
+      .then((res) => res.data);
+  };
+
+  put = (data?: T, id?: string) => {
+    return axiosInstance
+      .put<D>(this.endpoint + `/${id}`, data, { withCredentials: true })
+      .then((res) => res.data);
+  };
+
+  getALL = () => {
+    return axiosInstance
+      .get<T>(this.endpoint, { withCredentials: true })
       .then((res) => res.data);
   };
 }
