0112e02f1d7e41df9966b182f98553069e149bf0
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-24 01:03:40 +0800

    image upload hook

--- client/src/hooks/useUploadImage.ts
+++ client/src/hooks/useUploadImage.ts
@@ -1,18 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import APIClient from "../services/userService";
+import APIClient, { CloudinaryRes } from "../services/userService";
 
-const imageUploadClient = new APIClient<FormData, string>("imageUpload");
+const imageUploadClient = new APIClient<FormData, CloudinaryRes>("imageUpload");
+const saveImageClient = new APIClient<object, string>("users");
 
-const useUploadImage = () => {
-  return useMutation<string, AxiosError, FormData>({
+const useUploadImage = (imageCategory: string) => {
+  return useMutation<CloudinaryRes, AxiosError, FormData>({
     mutationFn: (formData: FormData) => {
       return imageUploadClient.post(formData, {
         "Content-Type": "multipart/form-data",
       });
     },
-    onSuccess: (imageURL: string) => {
-      console.log(imageURL);
+    onSuccess: async (imageURL: CloudinaryRes) => {
+      await saveImageClient
+        .put({ [imageCategory]: imageURL.imageUrl })
+        .then((res) => res)
+        .catch(() => {
+          throw new AxiosError("Failed to save image URL to user data");
+        });
     },
     onError: (error) => {
       console.log(error.message);
