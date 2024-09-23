2cbc9a544cb37d8b99b33800f6331df9408ca184
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-24 01:01:40 +0800

    refetch data for everyroute on navBar

--- client/src/component/navBar.tsx
+++ client/src/component/navBar.tsx
@@ -1,17 +1,37 @@
 import { Box, Button, HStack, Image, Show, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import SearchInput from "./SeachBar/SearchInput";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { useIsLoggedInStore, useMenuBarToggleStore } from "../store";
-import useUserInfo from "../hooks/useGetUser";
+import { useMenuBarToggleStore } from "../store";
+import { useIsLoggedInStore } from "../store";
+
+import { Avatar } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import useGetUser from "../hooks/useGetUser";
 
 const navBar = () => {
   const isOpen = useMenuBarToggleStore((s) => s.setIsOpen);
-  const { isLoading, error } = useUserInfo(); // should avoid auto retrying
+  const isLoggedIn = useIsLoggedInStore((s) => s.isLoggedIn);
+  // const userData = useIsLoggedInStore((s) => s.userData);
 
-  if (isLoading) return <></>;
+  const queryClient = useQueryClient();
+  const location = useLocation();
+
+  const setIsLoggedIn = useIsLoggedInStore((s) => s.setIsLoggedIn);
+  const setUserData = useIsLoggedInStore((s) => s.setUserData);
+  const { data, isLoading } = useGetUser(
+    setIsLoggedIn,
+    setUserData,
+    queryClient
+  ); // it returns error if user not auth
+
+  // Invalidate the query on route change
+  useEffect(() => {
+    queryClient.invalidateQueries(["userInfo"]);
+  }, [location.pathname]); // Invalidate on pathname change
 
   return (
     <HStack justifyContent="space-between" padding="10px">
@@ -31,9 +51,15 @@ const navBar = () => {
       <SearchInput></SearchInput>
 
       <Box padding={2}>
-        <Link to={error ? "/login" : "/profile"}>
-          <Text fontWeight="bold">{error ? "Login" : "Profile"}</Text>
-        </Link>
+        {!isLoading && (
+          <Link to={isLoggedIn ? "/profile" : "/login"}>
+            {isLoggedIn ? (
+              <Avatar name="Dan Abrahmov" src={data?.profileImage} />
+            ) : (
+              <Text fontWeight="bold">Login</Text>
+            )}
+          </Link>
+        )}
       </Box>
 
       <Show below="lg">
