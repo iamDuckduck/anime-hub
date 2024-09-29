a0291238b1a5f32425bda2344ae1ffed8ec1aefa
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-29 18:24:55 +0800

    fix both vercel json and backend cors setting

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,13 +2,17 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  // if (process.env.NODE_ENV == "development") {
-  // Set up CORS with custom options
-  const corsOptions = {
-    origin: "*", // Allow requests from this origin
-    methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-  // }
+  if (process.env.NODE_ENV == "development") {
+    // Set up CORS with custom options
+    const corsOptions = {
+      origin:
+        process.env.NODE_ENV == "development"
+          ? "http://localhost:5173"
+          : "https://anime-hub-one-chi.vercel.app",
+      // Allow requests from this origin
+      methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
+      credentials: true,
+    };
+    app.use(cors(corsOptions));
+  }
 };
