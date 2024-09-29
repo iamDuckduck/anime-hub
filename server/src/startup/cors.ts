cbbe2d1567687dcfc7b3ba284f400c64bc32ad32
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-29 18:12:43 +0800

    enable cors no matter what node env

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,13 +2,13 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  if (process.env.NODE_ENV == "development") {
-    // Set up CORS with custom options
-    const corsOptions = {
-      origin: "*", // Allow requests from this origin
-      methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-      credentials: true,
-    };
-    app.use(cors(corsOptions));
-  }
+  // if (process.env.NODE_ENV == "development") {
+  // Set up CORS with custom options
+  const corsOptions = {
+    origin: "*", // Allow requests from this origin
+    methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  // }
 };
