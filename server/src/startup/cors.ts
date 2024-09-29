79fe8231bf433818e6f42829726cf78073325992
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-29 18:09:22 +0800

    solve cors problem

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -5,7 +5,7 @@ export const enableCors = function (app: Express) {
   if (process.env.NODE_ENV == "development") {
     // Set up CORS with custom options
     const corsOptions = {
-      origin: "http://localhost:5173", // Allow requests from this origin
+      origin: "*", // Allow requests from this origin
       methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
       credentials: true,
     };
