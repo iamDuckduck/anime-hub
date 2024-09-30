121b5058b4ddadaf4ab93fcf4628eed0401eb0ff
iamDuckduck <simonchungCCK42@gmail.com>
2024-09-30 12:46:32 +0800

    attach onclick setislogged in for home and profile/login link

--- client/src/component/SideBar.tsx
+++ client/src/component/SideBar.tsx
@@ -1,7 +1,7 @@
 import { Box, CloseButton, Flex, HStack, Show, VStack } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
 import GenreList from "./GenreList";
-import { useMenuBarToggleStore } from "../store";
+import { useIsLoggedInStore, useMenuBarToggleStore } from "../store";
 
 const SideBar = () => {
   const location = useLocation();
@@ -9,13 +9,23 @@ const SideBar = () => {
   const isOpen = useMenuBarToggleStore((s) => s.isOpen);
   const setIsOpen = useMenuBarToggleStore((s) => s.setIsOpen);
 
+  const isLoggedIn = useIsLoggedInStore((s) => s.isLoggedIn);
+
   return (
     <Flex justifyContent="space-between">
       <VStack paddingY={2} align="flex-start">
         {isOpen && (
-          <Link to="/" onClick={() => setIsOpen(false)}>
-            Home
-          </Link>
+          <>
+            <Link to="/" onClick={() => setIsOpen(false)}>
+              <Box fontSize="2xl">Home</Box>
+            </Link>
+            <Link
+              to={isLoggedIn ? "/profile" : "/login"}
+              onClick={() => setIsOpen(false)}
+            >
+              <Box fontSize="2xl">{isLoggedIn ? "Profile" : "Login"}</Box>
+            </Link>
+          </>
         )}
         <Link onClick={() => setIsOpen(false)} to="/animes/seasons">
           <Box fontSize="2xl">Season</Box>
@@ -27,7 +37,11 @@ const SideBar = () => {
       </VStack>
 
       {isOpen && (
-        <CloseButton onClick={() => setIsOpen(false)} justifyContent="end" />
+        <CloseButton
+          padding={5}
+          onClick={() => setIsOpen(false)}
+          justifyContent="end"
+        />
       )}
     </Flex>
   );
